a371ec8a46cdf342f7b5ac54f2ee40951e4a430f
RubberDuckCollector <eggsim49@gmail.com>
2026-03-07 23:47:48 +0000

    added a shell script that builds spanish aqa `master_set.txt` and changed some spelling

---
+++ languages/spanish/aqa/make_master_set.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# this file should not be moved.
+# execute this file by calling its name to rebuild the master set of
+# vocabulary.
+
+cat ./GCSE/unit1_me_my_family_and_friends.txt ./GCSE/unit2_technology_in_everyday_life.txt ./GCSE/unit3_free_time_activities.txt ./GCSE/unit4_customs_and_festivals.txt ./GCSE/unit5_home_town_neighbourhood_and_region.txt ./GCSE/unit6_social_issues.txt ./GCSE/unit7_global_issues.txt ./GCSE/unit8_travel_and_tourism.txt ./GCSE/unit9_my_studies.txt ./GCSE/unit10_life_at_school_college.txt ./GCSE/unit11_education_post_16.txt ./GCSE/unit12_jobs_career_choices_and_ambitions.txt ./AS/unit1_los_valores_tradicionales_y_modernos.txt ./AS/unit2_el_ciberespacio.txt ./AS/unit3_la_igualdad_de_los_sexos.txt ./AS/unit4_la_influencia_de_los_ídolos.txt ./A2/unit5_monarquías_y_dictaduras.txt ./AS/unit6_el_patrimonio_cultural.txt ./A2/unit1_la_inmigración.txt ./A2/unit2_el_racismo.txt ./A2/unit3_la_convivencia.txt ./A2/unit4_jóvenes_del_hoy_ciudadanos_del_mañana.txt ./A2/unit5_monarquías_y_dictaduras.txt ./A2/unit6_los_movimientos_populares.txt ./A2/requiem-words.txt ./A2/idioms.txt ./A2/verbs-with-and-without-prepositions.txt ./general/beauty.txt ./general/body-parts.txt ./general/flowers.txt ./general/hard-words.txt ./general/numbers.txt ./general/phrases-that-trigger-the-subjunctive.txt > master_set.txt
